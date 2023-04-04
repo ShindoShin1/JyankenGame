00788b66fcd49b3a3500913bbea08a63ca0b0378
真藤真一 <128806885+ShindoShin1@users.noreply.github.com>
2023-04-04 13:35:05 +0900

    Add files via upload

--- js/player.js
+++ js/player.js
@@ -21,8 +21,13 @@ const sub_skills =
         ["勇気の光",1]  //使用時、相手に勝った時に与えるダメージ+2     
     ];
 
-var player_status = character;
-var enemy_status = character;
+var player_status = {};
+var enemy_status = {};
+
+for (let key in character) {
+    player_status[key] = character[key];
+    enemy_status[key] = character[key];
+}
 
 function create_player (characterCode) {
     player_status.name = 'player';
@@ -33,17 +38,41 @@ function create_player (characterCode) {
 
 
 function create_enemy (characterCode) {
-    player_status.name = 'computer';
-    player_status.type = 0;
-    player_status.main_skill = 1;
-    player_status.sub_skill = 1;
+
+    enemy_status.name = 'computer';
+    enemy_status.type = enemy_Random();
+    enemy_status.main_skill = 1;
+    enemy_status.sub_skill = 1;
 }
 
 function display_status() {
-    document.getElementById("player_type").textContent =hands_sign[player_status.type];
-    document.getElementById("player_skill1").textContent =main_skills[player_status.main_skill][0];
-    document.getElementById("player_skill2").textContent =sub_skills[player_status.sub_skill][0];
-    document.getElementById("enemy_type").textContent =hands_sign[enemy_status.type];
-    document.getElementById("enemy_skill1").textContent =main_skills[enemy_status.main_skill][0];
-    document.getElementById("enemy_skill2").textContent =sub_skills[enemy_status.sub_skill][0];
+    const player_type_area = document.getElementById("player_type");
+    const player_skill1_area = document.getElementById("player_skill1");
+    const player_skill2_area = document.getElementById("player_skill2");
+    const enemy_type_area = document.getElementById("enemy_type");
+    const enemy_skill1_area = document.getElementById("enemy_skill1");
+    const enemy_skill2_area = document.getElementById("enemy_skill2");
+    
+    //アイコン表示用
+    const player_hand_icon = document.createElement("i");
+    const enemy_hand_icon = document.createElement("i");
+    player_hand_icon.classList.add(hands_sign2[player_status.type][0]);
+    player_hand_icon.classList.add(hands_sign2[player_status.type][1]);
+    enemy_hand_icon.classList.add(hands_sign2[enemy_status.type][0]);
+    enemy_hand_icon.classList.add(hands_sign2[enemy_status.type][1]);
+
+    //document.getElementById("player_type").textContent =hands_sign[player_status.type];
+    if (player_type_area.children.length != 0){
+        player_type_area.removeChild(player_type_area.firstElementChild);
+    }
+    player_type_area.appendChild(player_hand_icon);
+    player_skill1_area.textContent =main_skills[player_status.main_skill][0];
+    player_skill2_area.textContent =sub_skills[player_status.sub_skill][0];
+    //document.getElementById("enemy_type").textContent =hands_sign[enemy_status.type];
+    if (enemy_type_area.children.length != 0){
+        enemy_type_area.removeChild(enemy_type_area.firstElementChild);
+    }
+    enemy_type_area.appendChild(enemy_hand_icon);
+    enemy_skill1_area.textContent =main_skills[enemy_status.main_skill][0];
+    enemy_skill2_area.textContent =sub_skills[enemy_status.sub_skill][0];
 }
